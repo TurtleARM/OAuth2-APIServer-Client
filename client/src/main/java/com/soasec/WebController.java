e3e30f7f07ee1c4373f428e3e3765aa52cc8c7eb
Ornaghi Davide <ornaghi@intre.it>
2020-11-11 00:26:34 +0100

    Fetch resources using access token

--- client/src/main/java/com/soasec/WebController.java
+++ client/src/main/java/com/soasec/WebController.java
@@ -1,5 +1,8 @@
 package com.soasec;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.springframework.boot.SpringApplication;
@@ -21,7 +24,7 @@ public class WebController {
 
     @GetMapping("/callbackPage")
     @ResponseBody
-    public String callbackPage(@RequestParam(required = false) String code) {
+    public String callbackPage(@RequestParam(required = false) String code) throws ParseException {
         // clientapp:strongpassword
         if (code != null) { // Auth code
             Connection.Response res;
@@ -42,7 +45,7 @@ public class WebController {
                 System.out.println("Cannot connect to the Authorization Server");
                 return "Error: " + ex;
             }
-            return res.body();
+            return printResponse(res);
         } else {
             return "Implicit grant type: Access token is in the URL";
         }
@@ -50,7 +53,7 @@ public class WebController {
 
     @GetMapping("/refreshToken")
     @ResponseBody
-    public String refreshToken(@RequestParam String token) {
+    public String refreshToken(@RequestParam String token) throws ParseException {
         Connection.Response res;
         try {
             res = Jsoup
@@ -68,12 +71,12 @@ public class WebController {
             System.out.println("Cannot connect to the Authorization Server");
             return "Error: " + ex;
         }
-        return res.body();
+        return printResponse(res);
     }
 
     @GetMapping("/password")
     @ResponseBody
-    public String password(@RequestParam String username, @RequestParam String password) {
+    public String password(@RequestParam String username, @RequestParam String password) throws ParseException {
         Connection.Response res;
         try {
             res = Jsoup
@@ -92,7 +95,7 @@ public class WebController {
             System.out.println("Cannot connect to the Authorization Server");
             return "Error: " + ex;
         }
-        return res.body();
+        return printResponse(res);
     }
 
     @GetMapping("/clientCredentials")
@@ -115,5 +118,27 @@ public class WebController {
             return "Error: " + ex;
         }
         return res.body();
+    }
+
+    private String printResponse(Connection.Response res) throws ParseException {
+        String result = res.body();
+        JSONParser parser = new JSONParser();
+        JSONObject json = (JSONObject) parser.parse(result);
+        String accessToken = (String) json.get("access_token");
+        try {
+            res = Jsoup
+                    .connect("https://localhost:8443/" + URLEncoder.encode("api", "UTF-8") + "/"
+                            + URLEncoder.encode("users", "UTF-8") + "/" + URLEncoder.encode("me", "UTF-8"))
+                    .header("authorization", "Bearer " + accessToken)
+                    .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
+                    .ignoreContentType(true)
+                    .ignoreHttpErrors(true)
+                    .method(Connection.Method.GET)
+                    .execute();
+            return "<p>" + result + "</p><p>Risposta Resource Server: " + res.body() + "</p>";
+        } catch (IOException ex) {
+            System.out.println("Cannot connect to the Authorization Server");
+            return "Error: " + ex;
+        }
     }
 }
